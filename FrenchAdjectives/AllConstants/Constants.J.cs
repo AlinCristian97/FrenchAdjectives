63862fd8ac0f069a55e532af5ebd80b4c600c4de
Alin <alin.cristian977@gmail.com>
2026-04-30 18:59:09 +0300

    j

--- FrenchAdjectives/AllConstants/Constants.J.cs
+++ FrenchAdjectives/AllConstants/Constants.J.cs
@@ -10,4 +10,19 @@ public partial class Constants
     public const string Joyeux = "joyeux";
     public const string Juste = "juste";
     public const string Juteux = "juteux";
+    public const string Jaillissant = "jaillissant";
+    public const string Jovial = "jovial";
+    public const string Judicieux = "judicieux";
+    public const string Justifie = "justifié";
+    public const string Jaunatre = "jaunâtre";
+    public const string Jobard = "jobard";
+    public const string Joueur = "joueur";
+    public const string Journalier = "journalier";
+    public const string Jubilant = "jubilant";
+    public const string Juvenile = "juvénile";
+    public const string Jacassier = "jacassier";
+    public const string Jalousant = "jalousant";
+    public const string Janissaire = "janissaire";
+    public const string Jaseur = "jaseur";
+    public const string Joufflu = "joufflu";
 }
