8230092da466503afb2748b26db0b540328b61a6
Alin <alin.cristian977@gmail.com>
2026-04-30 19:28:20 +0300

    o

--- FrenchAdjectives/AllConstants/Constants.O.cs
+++ FrenchAdjectives/AllConstants/Constants.O.cs
@@ -12,4 +12,19 @@ public partial class Constants
     public const string Optimiste = "optimiste";
     public const string Ouvert = "ouvert";
     public const string Oisif = "oisif";
+    public const string Obstine = "obstiné";
+    public const string Obsequieux = "obséquieux";
+    public const string Offensant = "offensant";
+    public const string Ombrageux = "ombrageux";
+    public const string Opaque = "opaque";
+    public const string Oppressant = "oppressant";
+    public const string Orgueilleux = "orgueilleux";
+    public const string Ose = "osé";
+    public const string Outrancier = "outrancier";
+    public const string Ovationne = "ovationné";
+    public const string Objectif = "objectif";
+    public const string Oblique = "oblique";
+    public const string Obsolete = "obsolète";
+    public const string Odieux = "odieux";
+    public const string Onctueux = "onctueux";
 }
