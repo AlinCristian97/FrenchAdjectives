c5a61713cae47382ad27b9e7cb878cb081e16335
Alin <alin.cristian977@gmail.com>
2026-04-30 19:56:13 +0300

    u

--- FrenchAdjectives/AllConstants/Constants.U.cs
+++ FrenchAdjectives/AllConstants/Constants.U.cs
@@ -7,4 +7,19 @@ public partial class Constants
     public const string Urgent = "urgent";
     public const string Usuel = "usuel";
     public const string Utile = "utile";
+    public const string Unanime = "unanime";
+    public const string Ultime = "ultime";
+    public const string Ulterieur = "ultérieur";
+    public const string Uniforme = "uniforme";
+    public const string Unilateral = "unilatéral";
+    public const string Unitaire = "unitaire";
+    public const string Usurpateur = "usurpateur";
+    public const string Utopique = "utopique";
+    public const string Ubiquitaire = "ubiquitaire";
+    public const string Unilingue = "unilingue";
+    public const string Univoque = "univoque";
+    public const string Urbanise = "urbanisé";
+    public const string Use = "usé";
+    public const string Utilitaire = "utilitaire";
+    public const string Umbrageux = "ombrageux";
 }
