a7ac95278227ea11060d7e9287f111391a39df9b
Alin <alin.cristian977@gmail.com>
2026-04-30 22:21:04 +0300

    Update AdjectiveRepository.H.cs

--- FrenchAdjectives/AllAdjectiveRepository/AdjectiveRepository.H.cs
+++ FrenchAdjectives/AllAdjectiveRepository/AdjectiveRepository.H.cs
@@ -31,4 +31,28 @@ internal static partial class AdjectiveRepository
         Constants.Hieratique,
         Constants.Homogene,
     };
+
+    public static readonly IReadOnlyList<string> H_Popular = new[]
+    {
+        Constants.Heureux,
+        Constants.Honnete,
+        Constants.Horrible,
+        Constants.Humble,
+        Constants.Humain,
+        Constants.Habile,
+        Constants.Hautain,
+        Constants.Hostile,
+        Constants.Honorable,
+        Constants.Harmonieux,
+        Constants.Hilarant,
+        Constants.Hesitant,
+        Constants.Heroique,
+        Constants.Hypocrite,
+        Constants.Hyperactif,
+        Constants.Hargneux,
+        Constants.Hallucinant,
+        Constants.Hasardeux,
+        Constants.Hatif,
+        Constants.Hagard,
+    };
 }
