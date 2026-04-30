2c3796ef9712232934c28136ea9db3d588b35e71
Alin <alin.cristian977@gmail.com>
2026-04-30 18:37:21 +0300

    f

--- FrenchAdjectives/AllConstants/Constants.F.cs
+++ FrenchAdjectives/AllConstants/Constants.F.cs
@@ -29,4 +29,19 @@ public partial class Constants
     public const string Ferme = "fermé";
     public const string Fragile = "fragile";
     public const string Fringant = "fringant";
+    public const string Fabuleux = "fabuleux";
+    public const string Fade = "fade";
+    public const string Fantastique = "fantastique";
+    public const string Fataliste = "fataliste";
+    public const string Favorable = "favorable";
+    public const string Feroce = "féroce";
+    public const string Fertile = "fertile";
+    public const string Fiable = "fiable";
+    public const string Flamboyant = "flamboyant";
+    public const string Flatteur = "flatteur";
+    public const string Fougueux = "fougueux";
+    public const string Franc = "franc";
+    public const string Frenetique = "frénétique";
+    public const string Frivole = "frivole";
+    public const string Fulgurant = "fulgurant";
 }
