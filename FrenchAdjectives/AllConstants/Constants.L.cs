29257c0291abef7c9e090f91e6511899f079fe8f
Alin <alin.cristian977@gmail.com>
2026-04-30 19:11:42 +0300

    l

--- FrenchAdjectives/AllConstants/Constants.L.cs
+++ FrenchAdjectives/AllConstants/Constants.L.cs
@@ -19,4 +19,19 @@ public partial class Constants
     public const string Legendaire = "légendaire";
     public const string Limite = "limité";
     public const string Leste = "leste";
+    public const string Lacunaire = "lacunaire";
+    public const string Langoureux = "langoureux";
+    public const string Lascif = "lascif";
+    public const string Latent = "latent";
+    public const string Laudatif = "laudatif";
+    public const string Lenifiant = "lénifiant";
+    public const string Limpide = "limpide";
+    public const string Loquace = "loquace";
+    public const string Lucide = "lucide";
+    public const string Lugubre = "lugubre";
+    public const string Luxuriant = "luxuriant";
+    public const string Lyrique = "lyrique";
+    public const string Laborieux = "laborieux";
+    public const string Lapidaire = "lapidaire";
+    public const string Litigieux = "litigieux";
 }
