9b160dfd016bcd27ac50e6f3083f7745bf502657
Alin <alin.cristian977@gmail.com>
2026-04-30 19:20:02 +0300

    n

--- FrenchAdjectives/AllConstants/Constants.N.cs
+++ FrenchAdjectives/AllConstants/Constants.N.cs
@@ -15,4 +15,19 @@ public partial class Constants
     public const string Noir = "noir";
     public const string Nouveau = "nouveau";
     public const string Nouvelle = "nouvelle";
+    public const string Naif = "naïf";
+    public const string Narquois = "narquois";
+    public const string Nocturne = "nocturne";
+    public const string Noble = "noble";
+    public const string Nefaste = "néfaste";
+    public const string Nonchalant = "nonchalant";
+    public const string Nostalgique = "nostalgique";
+    public const string Nuance = "nuancé";
+    public const string Negatif = "négatif";
+    public const string Novateur = "novateur";
+    public const string Nebuleux = "nébuleux";
+    public const string Naufrage = "naufragé";
+    public const string Neutre = "neutre";
+    public const string Nitide = "nitide";
+    public const string Nomade = "nomade";
 }
