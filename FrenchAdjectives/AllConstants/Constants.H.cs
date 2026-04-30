fe4578e73338a14eff9e50c1f2ae2b74cd929038
Alin <alin.cristian977@gmail.com>
2026-04-30 18:47:41 +0300

    h

--- FrenchAdjectives/AllConstants/Constants.H.cs
+++ FrenchAdjectives/AllConstants/Constants.H.cs
@@ -11,4 +11,19 @@ public partial class Constants
     public const string Humain = "humain";
     public const string Humide = "humide";
     public const string Haute = "haute";
+    public const string Harmonieux = "harmonieux";
+    public const string Hasardeux = "hasardeux";
+    public const string Heroique = "héroïque";
+    public const string Hesitant = "hésitant";
+    public const string Hilarant = "hilarant";
+    public const string Hostile = "hostile";
+    public const string Humble = "humble";
+    public const string Hyperactif = "hyperactif";
+    public const string Hypocrite = "hypocrite";
+    public const string Hagard = "hagard";
+    public const string Hallucinant = "hallucinant";
+    public const string Hargneux = "hargneux";
+    public const string Hatif = "hâtif";
+    public const string Hieratique = "hiératique";
+    public const string Homogene = "homogène";
 }
