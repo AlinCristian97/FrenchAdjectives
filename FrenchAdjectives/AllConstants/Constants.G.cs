452d46bb4627e817aa449088e3132647df620c38
Alin <alin.cristian977@gmail.com>
2026-04-30 18:43:39 +0300

    g

--- FrenchAdjectives/AllConstants/Constants.G.cs
+++ FrenchAdjectives/AllConstants/Constants.G.cs
@@ -12,4 +12,19 @@ public partial class Constants
     public const string Grand = "grand";
     public const string Gros = "gros";
     public const string Grisonnant = "grisonnant";
+    public const string Gracieux = "gracieux";
+    public const string Grandiose = "grandiose";
+    public const string Grave = "grave";
+    public const string Gregaire = "grégaire";
+    public const string Grimacier = "grimacier";
+    public const string Grognon = "grognon";
+    public const string Grotesque = "grotesque";
+    public const string Guerrier = "guerrier";
+    public const string Guide = "guidé";
+    public const string Gaillard = "gaillard";
+    public const string Galant = "galant";
+    public const string Gargantuesque = "gargantuesque";
+    public const string Givre = "givré";
+    public const string Glorieux = "glorieux";
+    public const string Gourmand = "gourmand";
 }
